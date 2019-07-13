b0497ff057e692bda1b696e6d16304654949a067
Cory <cory@dexai.com>
2019-07-13 14:28:01 -0400

    first commit of technical interview work

---
+++ CodingSample.py
@@ -0,0 +1,16 @@
+
+#currently treating country code as mandatory. 
+#May change mind on that later.
+def numbers_to_words(number_string):
+	if len(number_string) != 11 or number_string[0]!=1:
+		return('Error. Check phone number. digits missing? Country code ok?')
+
+	pass
+
+
+def words_to_numbers(wordification):
+	pass
+
+
+def all_wordifications(number_string):
+	pass
