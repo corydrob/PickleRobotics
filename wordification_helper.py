298d5e28437f3a9b87363959881f197572e1ad7e
Cory <cory@dexai.com>
2019-07-17 21:40:41 -0400

    began file of helper functions. Contains dictionaries for number->letter and letter->number lookup

---
+++ wordification_helper.py
@@ -0,0 +1,13 @@
+keypad={'2':['A','B','C'],
+	'3':['D','E','F'],
+	'4':['G','H','I'],
+	'5':['J','K','L'],
+	'6':['M','N','O'],
+	'7':['P','Q','R','S'],
+	'8':['T','U','V'],
+	'9':['W','X','Y','Z']}
+
+reverse_keypad={}
+for number,letters in keypad.items():
+	for char in letters:
+		reverse_keypad[char]=number
