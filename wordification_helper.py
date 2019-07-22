1851d02df08b3a1f7bdd7aafae4ea53fe4dfa9c8
Cory <cory@dexai.com>
2019-07-22 19:35:21 -0400

    Added comment in file.

--- wordification_helper.py
+++ wordification_helper.py
@@ -14,3 +14,28 @@ for number,letters in keypad.items():
 
 #print('keypad: \n', keypad)
 #print('reverse lookup: \n',reverse_keypad)
+
+'''
+After I wrote parse_fragments I realized that this does not work 
+because i made the bad assumption that words would be number-
+separated
+
+'''
+def parse_fragments(s):
+	'''
+	Takes a string, s, of letters and numbers and returns a list of 
+	letter-chunks.
+	does NOT consult a dictionary to see if they are real words
+	'''
+	
+	frags=[]		#list of letter fragments to be returned. 
+	chunk=''		#current run of letters in string not interrupted by number
+	for char in s: 
+		if char.isalpha():
+			chunk+=char
+		else:
+			frags.append(chunk)
+			chunk=''
+	if len(chunk)>0:
+		frags.append(chunk)
+	return frags
